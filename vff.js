5f3b7d78feffb4a5ee4d9cf113c4f1d7cab9a6ba
sophiavanvalkenburg <sophiavanvalkenburg@users.noreply.github.com>
2018-02-11 02:59:32 -0500

    use clipping plane to clip fossils

--- vff.js
+++ vff.js
@@ -2,6 +2,7 @@ var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 var renderer = new THREE.WebGLRenderer({antialias: true});
+renderer.localClippingEnabled = true;
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
@@ -20,38 +21,41 @@ scene.add( light );
 camera.position.set(0, 10, 0)
 camera.lookAt(0, 0, 0);
 
-var material = new THREE.MeshLambertMaterial( { color: 0xE3DAC9 } );
-var positions = getPositions();
+//var positions = getPositions();
 var meshes = [];
 
 var loader = new THREE.STLLoader();
-loader.load( './models/AMNH-FI-101649_M21284-40437__thorax.stl', function ( loadedGeometry ) {
-    for (var i = 0; i < 18; i++){
+loader.load( './models/AMNH-FI-101479_M21282-40430__cephalon.stl', function ( loadedGeometry ) {
+    //for (var i = 0; i < 18; i++){
+        var localPlane = getRandomClippingPlane();
+        var localPlane2 = getRandomClippingPlane();
+        var material = new THREE.MeshLambertMaterial( { color: 0xE3DAC9, clippingPlanes: [localPlane, localPlane2]} );
         var mesh = new THREE.Mesh( loadedGeometry, material);
-        mesh.scale.set(0.3, 0.3, 0.3);
-        var rotation = Math.random() * 360;
-        mesh.rotation.set(0, rotation, 0);
-        var pos = positions[i];
-        mesh.position.set(pos[0], pos[1], pos[2]);
+        mesh.scale.set(0.5, 0.5, 0.5);
+        var rotationY = Math.random() * 2 * Math.PI;
+        var rotationZ = (0.25 * Math.random() - 0.125) * 2 * Math.PI; 
+        mesh.rotation.set(0, rotationY, rotationZ);
+        //var pos = positions[i];
+        //mesh.position.set(pos[0], pos[1], pos[2]);
         meshes.push(mesh);
         scene.add( mesh );
-    }
+    //}
 });
 
-var getImageData = true;
-var doSplit = false;
-function animate() {
-    requestAnimationFrame( animate );
-    renderer.render( scene, camera );
-    if (meshes.length > 0 && getImageData){
-        imgData = renderer.domElement.toDataURL();
-        console.log(imgData);
-        getImageData = false;
-        doSplit = true;
+function getRandomClippingPlane() {
+    // pick clipping axis
+    var x, z;
+    if (Math.random() > 0.5){
+        x = 1;
+        z = 0;
+    } else {
+        x = 0;
+        z = 1;
     }
+    return new THREE.Plane( new THREE.Vector3( x, 0, z ), 2 * Math.random() - 1 );
 }
-animate();
 
+/*
 function getPositions(){
     var unit = 4;
     var offset = unit * 9;
@@ -63,3 +67,17 @@ function getPositions(){
     }
     return positions;
 }
+*/
+
+var getImageData = true;
+function animate() {
+    requestAnimationFrame( animate );
+    renderer.render( scene, camera );
+    if (meshes.length > 0 && getImageData){
+        imgData = renderer.domElement.toDataURL();
+        console.log(imgData);
+        getImageData = false;
+    }
+}
+animate();
+
